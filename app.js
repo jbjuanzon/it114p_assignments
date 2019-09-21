89fde06ba084c2c0de6792aecd0241660c654cf0
jbjuanzon <jbjuanzon2@yahoo.com>
2019-09-21 13:57:54 +0800

    Assignment 4 - Make use of CSS Framework to our web project

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bulma/css')));
+app.use('/font', express.static(path.join(__dirname, 'node_modules/@mdi/font')));
 
 const accountRoutes = require('./routes/userAccount')
 const viewRoutes = require('./routes/viewUser')
